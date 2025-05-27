887a1b2e19fbf18af074e05c3a6e39d0aa6dafc7
Tooni <fuadbashuaa@gmail.com>
2025-05-27 09:39:54 +0100

    starting to develop the logic behind the game

--- script.js
+++ script.js
@@ -1,20 +1,105 @@
-const Gameboard = (function () {
+// create function factories and then wrap it in an IIFE
+function Gameboard() {
     const board = []
+    const columns = 3;
+    const rows = 3;
 
-    for(let i = 0; i < 3; i++) {
+    // creating a 2d array w/ a nested for loop which represents the state of the gameboard
+    for(let i = 0; i < rows; i++) {
         board[i] = [];
-        for(let j = 0; j < 3; i++) {
-            
+        for(let j = 0; j < columns; j++) {
+            board[i].push[j];
         }
     }
 
+    // this method will get our entire board so that our Ui can render it
+    const getBoard = () => board;
 
-    return {
-        board
+    //
+    const dropToken = (column, player) => {
+    // here I am looping through rows to find all the rows that dont have a token
+    const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
+
+
+    if (!availableCells.length) return;
+
+    const lowestRow = availableCells.length - 1;
+    board[lowestRow][column].addToken(player)
+};
+
+    const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithCellValues);
+};
+
+    return { 
+        getBoard, 
+        dropToken, 
+        printBoard 
+    };
+   
+}
+
+
+function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
     };
 
-    
-})();
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue
+    };
+}
+
+
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+    const board = Gameboard()
+
+    const players = [
+        {
+            name: playerOneName,
+            token: 1
+        },
+        {
+            name: playerTwoName,
+            token: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+       const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playRound = (column) => {
+        console.log(`Dropping ${getActivePlayer().name}'s token into column ${column}...`);
+        board.dropToken(column, getActivePlayer().token);
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
+
+const game = GameController();
 
 const displayController = (function () {
 
@@ -29,7 +114,5 @@ function player(name, marker) {
     return user;
 }
 
-const player1 = player('Sam', 'O')
-console.log(player1)
 
 
